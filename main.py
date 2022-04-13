84fd4964f21eb3f2d47e66ef333546392e2e94f6
daxmahoney@gmail.com <daxmahoney@gmail.com>
2022-04-12 20:35:55 -0500

    delete function added

--- main.py
+++ main.py
@@ -41,3 +41,25 @@ async def get_shape_by_id(shape_id:int):
 async def post_shape(shape: Shape):
     shapes.insert_one(shape.dict())
     return shape
+
+
+@app.put("/shapes/{shape_id}")
+async def update_shape(shape_id: int, shape: Shape):
+    
+    if shapes.count_documents({"id": shape_id}) > 0:
+        # filer document to find a shape with a specific id
+        shapes.replace_one({"id": shape_id}, shape.dict())
+        return shape
+    raise HTTPException(status_code=404, detail=f"No shape with id {shape_id} found")
+
+@app.put("/shapes/upsert/{shape_id}")
+async def update_shape(shape_id: int, shape: Shape):
+    shapes.replace_one({"id": shape_id}, shape.dict(), upsert=True)
+    return shape
+
+@app.delete("/shapes/{shape_id")
+async def delete_shape(shape_id: int):
+    delete_result = shapes.delete_one({"id": shape_id})
+    if delete_result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail=f"No shape with {shape_id} exists")
+    return{"OK": True}
